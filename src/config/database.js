5bfa4e2785573548e0aee7720599127151d87442
klevessonroseno <klevessonroseno42@gmail.com>
2020-04-24 18:56:46 -0300

    Migration de Customer

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    database: 'db_customer_portfolio',
+    username: 'root',
+    password: '',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+
+};
