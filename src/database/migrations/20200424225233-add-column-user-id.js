ce2938eaa5abe8d7cb2f766b87fb9c96c239d813
klevessonroseno <klevessonroseno42@gmail.com>
2020-04-24 20:00:31 -0300

    migration para adicionar chave estrageira na tabela customers

---
+++ src/database/migrations/20200424225233-add-column-user-id.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+      return queryInterface.addColumn(
+        'customers',
+        'user_id',
+        {
+          type: Sequelize.INTEGER,
+          references: { model: 'users', key: 'id' },
+          onUpdate: 'CASCADE',
+          onDelete: 'SET NULL',
+          allowNull: true,
+      });    
+  },
+
+  down: queryInterface => {
+      return queryInterface.removeColumn('customers', 'user_id');
+  }
+};
