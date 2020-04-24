20cde0c5398fb0a8dec8ba0990a770c90e3066ab
klevessonroseno <klevessonroseno42@gmail.com>
2020-04-24 20:07:13 -0300

    Model de usuário

---
+++ src/models/User.js
@@ -0,0 +1,17 @@
+import Sequelize, { Model } from 'sequelize';
+
+class User extends Model {
+    static init(connection){
+        super.init({
+            name: Sequelize.STRING,
+            email: Sequelize.STRING,
+            password: Sequelize.VIRTUAL,
+            password_hash: Sequelize.STRING,
+        }, {
+            sequelize: connection,
+        });
+        return this;
+    }
+}
+
+export default User;
