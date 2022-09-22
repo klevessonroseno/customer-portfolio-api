799997cff1d9fd957c3588436729a760f58be03a
klevessonroseno <klevesson.developer@gmail.com>
2022-09-22 14:53:21 -0300

    Checking if email already registered before saving new user.

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -15,7 +15,15 @@ class UserController{
                     error: 'Validation Fails',
                 });
             }
+        
+            const userExists = await User.findOne({
+                where: { email: req.body.email }
+            });
 
+            if(userExists) return res.status(400).json({
+                message: 'E-mail already registered.'
+            });
+            
             const { name, email } = await User.create(req.body);
             
             return res.status(201).json({ name, email });
