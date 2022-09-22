8202a124d84098eb5832d163b6af2976f2f52697
klevessonroseno <klevesson.developer@gmail.com>
2022-09-22 14:21:52 -0300

    Finished connection to new database

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,14 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
+require('dotenv').config();
 
 module.exports = {
-    dialect: process.env.DIALECT,
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
+    dialect: process.env.DATABASE_DIALECT,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
     define: {
         timestamps: true,
         underscored: true,
         underscoredAll: true,
     },
-
 };
